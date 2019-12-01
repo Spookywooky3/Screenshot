83f900fbcec4a9f54f2638d2dc5ace5f606941a3
Spookywooky3 <33356007+Spookywooky3@users.noreply.github.com>
2019-12-01 19:17:55 +0800

    New function

--- Screenshot_Test/Program.cs
+++ Screenshot_Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing.Imaging;
+using System.Drawing;
 
 namespace Screenshot_Test
 {
@@ -7,7 +8,13 @@ namespace Screenshot_Test
         private static void Main(string[] args)
         {
             Screenshot.Screenshot screenshot = new Screenshot.Screenshot();
-            screenshot.TakeScreenshot(1920, 1080, true, true, @"D:\\test.jpg", ImageFormat.Jpeg);
+
+            /* Take the screenshot without any return */
+            screenshot.TakeScreenshot(1920, 1080, true, true, @"D:\test.jpg", ImageFormat.Jpeg);
+
+            Bitmap bitmap = screenshot.TakeBitmapScreenshot(1920, 1080);
+            /* Do whatever you want with the bitmap */
+            bitmap.Save(@"D:\test.jpg", ImageFormat.Jpeg);
         }
     }
 }
