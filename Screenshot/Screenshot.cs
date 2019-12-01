d3332b70467262c26ac18aa511f269a34a2a4afc
Spookywooky3 <33356007+Spookywooky3@users.noreply.github.com>
2019-12-01 18:19:58 +0800

    First Commit

---
+++ Screenshot/Screenshot.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Screenshot
+{
+    /// <summary>
+    /// Use this class to take screenshots.
+    /// </summary>
+    public class Screenshot
+    {
+        /// <summary>
+        /// Take Screenshot.
+        /// </summary>
+        /// <param name="width">Width of the screenshot</param>
+        /// <param name="height">Height of the screenshot</param>
+        /// <param name="open">Open the screenshot?</param>
+        /// <param name="save">Save the screenshot?</param>
+        /// <param name="saveLocation">Location to save (Leave null if save is false).</param>
+        /// <param name="format">The ImageFormat to save as.</param>
+        /// <returns></returns>
+        public Bitmap TakeScreenshot(int width, int height, bool open, bool save, string saveLocation = null, ImageFormat format = null)
+        {
+            Rectangle size;
+            size.Width = width;
+            size.Height = height;
+            using (Bitmap bitmap = new Bitmap(width, height))
+            {
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.CopyFromScreen(new Point(size.Left, size.Top), Point.Empty, size.Size);
+
+                if (save == true)
+                {
+                    bitmap.Save(saveLocation, format);
+                }
+                if (open == true)
+                {
+                    bitmap.Save($"{Path.GetTempPath()}\\bitmap.jpg", ImageFormat.Jpeg);
+                    Process p = new Process();
+                    p.StartInfo.FileName = $"{Path.GetTempPath()}\\bitmap.jpg";
+                }
+                return bitmap;
+            }
+        }
+    }
+}
---
+++ Screenshot_Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing.Imaging;
+
+namespace Screenshot_Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Screenshot.Screenshot screenshot = new Screenshot.Screenshot();
+            screenshot.TakeScreenshot(1920, 1080, true, true, @"D:\\test.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
